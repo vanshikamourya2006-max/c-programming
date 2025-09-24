cfa943b5f09e55b1de742d66a225c813c3a73956
Vanshika Mourya <vanshikamourya2006@gmail.com>
2025-09-24 13:31:22 +0530

    spiral

--- lec19/spiral.c
+++ lec19/spiral.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
-int main()
-{
-    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int top=0,left=0;
-    int bottom=2,right=2;
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int top=0,right=2,bottom=2,left=0;
+    while(top<bottom&&left<right){
     for(int i=0;i<3;i++)
     {
-        printf("%d " ,a[top][i]);
+        printf("%d ",arr[top][i]);
     }
     top++;
     for(int i=top;i<=bottom;i++)
     {
-        
-        printf("%d " ,a[i][right]);
+        printf("%d ",arr[i][right]);
     }
     right--;
-    for(int i=bottom;i>=top;i--)
+    for (int i=right;i>=left;i--) 
     {
-        
-        printf("%d " ,a[i][left]);
+                printf("%d ", arr[bottom][i]);
+            }
+    bottom--;
+    for (int i=bottom;i>=top;i--)
+    {
+        printf("%d ", arr[i][left]);
     }
     left++;
     for(int i=left;i<=right;i++)
     {
-        
-        printf("%d " ,a[right][i]);
+        printf("%d ", arr[right][i]);
     }
+}
 }
