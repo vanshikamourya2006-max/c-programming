32b71abe172ca3017f111d5ade84319d69ac73d8
Vanshika Mourya <vanshikamourya2006@gmail.com>
2025-10-10 13:05:08 +0530

    array

--- lec12/arraymissing.c
+++ lec12/arraymissing.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 int  main()
 {
-    int a[5]={1,2,3,4,5};
-    
+    int n;
+    int a[100];
+    int m=0;
+    printf("enter no of elements =");
+    scanf("%d" ,&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int sum=0;
+    int originalsum=1;
+     for (int i=0;i<n;i++)
+     {
+        sum= sum+a[i];
+     }   
+     originalsum=((n+1)*(n+2))/2;
+     m=originalsum-sum;
+     printf(" missing no is %d" ,m);
+
 }
